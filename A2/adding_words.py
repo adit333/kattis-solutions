6f3ba79281ad60ce1014aec7c1082fc7b9eb21e9
Adit Rada <rada_adit@hotmail.com>
2022-01-11 16:02:32 -0700

    Working adding words

--- A2/adding_words.py
+++ A2/adding_words.py
@@ -2,54 +2,41 @@
 """
 
 import sys
-import operator
-from typing import Dict, List
+from typing import List
+
 
 def adding_words(input_lines: List[str]) -> None:
     variables = {}
     for input_line in input_lines:
         tokens = input_line.split()
         if tokens[0] == 'def':
-            variables[tokens[1]] = int(tokens[2])
+            variables[tokens[1]] = tokens[2]
+
         elif tokens[0] ==  'calc':
             print(' '.join(tokens[1:]), end=' ')
-            operand1 = int(variables.get(tokens[1], False))
-            operand2 = int(variables.get(tokens[3], False))
-            if operand1 and operand2:
-                if tokens[2] == '+':
-                    result = operand1 + operand2
-                elif tokens[2] == '-':
-                    result = operand1 + operand2
+            statement = []
+            unknown = False
+            for token in tokens[1:-1]:
+                if token in variables:
+                    statement.append(variables[token])
+                elif token == '+':
+                    statement.append('+')
+                elif token == '-':
+                    statement.append('-')
+                else:
+                    unknown = True
+                    print('unknown')
+                    break
+
+            if not unknown:
+                result = str(eval(''.join(statement)))
                 if result in variables.values():
                     print(list(variables.keys())[list(variables.values()).index(result)])
                 else:
                     print('unknown')
-            else:
-                print('unknown')
+
         elif tokens[0] ==  'clear':
             variables = {}
-        
-def get_result(tokens: List[str], variables: Dict[str, int]):
-    result = 0
-    i = 0
-    operators = {'+': operator.add, '-': operator.sub}
-    while i < len(tokens):
-        if tokens[i] in operators:
-            operation = operators.get(tokens[i])
-            operand = int(variables.get(tokens[i+1], False))
-            if operand:
-                result = result + operation(result, tokens[i+1])
-                i += 2
-            else:
-                return False
-        else:
-            operand = int(variables.get(tokens[i+1], False))
-            if operand:
-                result = result + operand
-                i += 1
-            else:
-                return False
-    return result
 
 
 def main():
@@ -59,6 +46,12 @@ def main():
                     'def bar 7', 
                     'def programming 10', 
                     'calc foo + bar = ',
+                    'def is 4',
+                    'def fun 8',
+                    'calc programming - is + fun =',
+                    'def fun 1',
+                    'calc programming - is + fun =',
+                    'clear',
     ]
     adding_words(input_lines)
 
