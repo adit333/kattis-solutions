f9420c0f09b5de2a01b8dfacda4e86f082b7e852
Adit Rada <rada_adit@hotmail.com>
2022-01-11 18:36:47 -0700

    Added c++ header

---
+++ header.cpp
@@ -0,0 +1,21 @@
+/*
+  Name: Adit Rada
+  Student ID: 1622972
+
+  List any resources you used below (eg. urls, name of the algorithm from our code archive).
+  Remember, you are permitted to get help with general concepts about algorithms
+  and problem solving, but you are not permitted to hunt down solutions to
+  these particular problems!
+
+  None
+
+  List any classmate you discussed the problem with. Remember, you can only
+  have high-level verbal discussions. No code should be shared, developed,
+  or even looked at in these chats. No formulas or pseudocode should be
+  written or shared in these chats.
+
+  None
+
+  By submitting this code, you are agreeing that you have solved in accordance
+  with the collaboration policy in CMPUT 403.
+*/
