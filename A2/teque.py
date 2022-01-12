67ece384b8f9e1dff854959205680299abbce303
Adit Rada <rada_adit@hotmail.com>
2022-01-11 21:57:43 -0700

    use stdout instead of print for teque, still tle

--- A2/teque.py
+++ A2/teque.py
@@ -7,33 +7,35 @@ from typing import List
 
 def teque(operations: List[str]) -> None:
     teque_ds = []
+    result = []
     for operation in operations:
         opcode, value = operation.split()
         if opcode == 'push_back':
-            teque_ds.append(int(value))
+            teque_ds.append(value)
         elif opcode == 'push_front':
-            teque_ds.insert(0, int(value))
+            teque_ds.insert(0, value)
         elif opcode == 'push_middle':
             mid_pos = math.ceil(len(teque_ds) / 2)
-            teque_ds.insert(mid_pos, int(value))
+            teque_ds.insert(mid_pos, value)
         elif opcode == 'get':
-            print(teque_ds[int(value)])
+            result.append(teque_ds[int(value)])
+    sys.stdout.write('\n'.join(result))
 
 def main():
-    input_lines = sys.stdin.read().splitlines()
-    teque(input_lines[1:])
-    # teque([
-    #     'push_back 9',
-    #     'push_front 3',
-    #     'push_middle 5',
-    #     'get 0',
-    #     'get 1',
-    #     'get 2',
-    #     'push_middle 1',
-    #     'get 1',
-    #     'get 2',
-    # ]
-    # )
+    # input_lines = sys.stdin.read().splitlines()
+    # teque(input_lines[1:])
+    teque([
+        'push_back 9',
+        'push_front 3',
+        'push_middle 5',
+        'get 0',
+        'get 1',
+        'get 2',
+        'push_middle 1',
+        'get 1',
+        'get 2',
+    ]
+    )
 
 if __name__ == '__main__':
     main()
