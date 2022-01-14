f01cb90548851d0e1ff7b3f9b5afb836dc4b83df
Adit Rada <rada_adit@hotmail.com>
2022-01-13 17:57:53 -0700

    Working teque, with no TLE

--- A2/teque.py
+++ A2/teque.py
@@ -1,25 +1,47 @@
 """https://ualberta.kattis.com/sessions/jot9wq/problems/teque
 """
 
-import math
 import sys
-from typing import List
+from collections import deque
+from typing import Deque, List
+
+
+def balance(teque_ds_left: Deque, teque_ds_right: Deque) -> None:
+    if (len(teque_ds_left) == len(teque_ds_right))  \
+        or (len(teque_ds_left) == len(teque_ds_right) + 1):
+        return
+    # If we are here, this means that there are odd no. of elements
+    # and either the left deque has 1 more element than it should
+    # or the right deque has one or more element than it should
+    if len(teque_ds_left) == len(teque_ds_right) + 2:   # Left deque has 1 extra element
+        teque_ds_right.appendleft(teque_ds_left.pop())
+    else:
+        teque_ds_left.append(teque_ds_right.popleft())
+
 
 def teque(operations: List[str]) -> None:
-    teque_ds = []
-    result = []
+    result = []     # Used to store the output so we can print it all at once
+    teque_ds_left = deque()
+    teque_ds_right = deque()
     for operation in operations:
         opcode, value = operation.split()
         if opcode == 'push_back':
-            teque_ds.append(value)
+            teque_ds_right.append(value)
+            balance(teque_ds_left, teque_ds_right)
         elif opcode == 'push_front':
-            teque_ds.insert(0, value)
+            teque_ds_left.appendleft(value)
+            balance(teque_ds_left, teque_ds_right)
         elif opcode == 'push_middle':
-            mid_pos = math.ceil(len(teque_ds) / 2)
-            teque_ds.insert(mid_pos, value)
+            teque_ds_left.append(value)
+            balance(teque_ds_left, teque_ds_right)
         elif opcode == 'get':
-            result.append(teque_ds[int(value)])
+            if int(value)  < len(teque_ds_left):
+                result.append(teque_ds_left[int(value)])
+            else:
+                right_index = int(value) - len(teque_ds_left)
+                result.append(teque_ds_right[right_index])
     sys.stdout.write('\n'.join(result))
+
 
 def main():
     # input_lines = sys.stdin.read().splitlines()
