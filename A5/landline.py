cc2d00d0ccbcd883844e41170b308c2c58c2f290
Adit Rada <rada_adit@hotmail.com>
2022-02-04 21:30:22 -0700

    minor fix to landline

--- A5/landline.py
+++ A5/landline.py
@@ -79,7 +79,7 @@ def landline(num_vertices: int, num_insecure_buildings: int,
     
     if num_edges_taken == num_vertices - 1:
         print(mst_cost)
-    elif num_edges_taken == 0 and num_insecure_buildings == 2:      # TODO: rectify this hack
+    elif num_edges_taken == 0 and num_insecure_buildings == 2:      # TODO: rectify this hack for special case of just 2 insecure buildings
         print(edge_list[0][2])
     else:
         print("impossible")
