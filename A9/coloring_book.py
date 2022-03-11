ed2f321939af7120000a748cfdde2e6752863c30
Adit Rada <rada_adit@hotmail.com>
2022-03-11 15:57:33 -0700

    working coloring book

---
+++ A9/coloring_book.py
@@ -0,0 +1,16 @@
+"""https://ualberta.kattis.com/sessions/c2gb3k/problems/colouringbook
+"""
+
+import sys
+
+def coloring_book() -> None:
+    pass
+
+
+def main():
+    input_lines = sys.stdin.read().splitlines()
+    num_of_points, num_of_lines = list(map(int, input_lines[0]))
+    
+
+if __name__ == '__main__':
+    main()
