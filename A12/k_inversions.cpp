de62881ed26a49c30a88fa15a663a531268e1835
Adit Rada <rada_adit@hotmail.com>
2022-04-20 21:44:58 -0600

    working LU factorizer

--- A12/k_inversions.cpp
+++ A12/k_inversions.cpp
@@ -1,95 +1,109 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string>
+/* https://ualberta.kattis.com/sessions/wuuqfx/problems/kinversions
+*/
 
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
 using namespace std;
 
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
 typedef long long ll;
 
-ll gcd(ll a, ll b, ll &s, ll &t) { // a*s+b*t = g
-  if (b==0) { t = 0; s = (a < 0) ? -1 : 1; return (a < 0) ? -a : a;
-  } else { ll g = gcd(b, a%b, t, s);  t -= a/b*s;  return g; }
+const ld  pi   = 4.0*atanl(1.0);
+const int iinf = 1e9 + 10;
+const ll  inf  = 1e18 + 10;
+const int mod  = 1000000007;
+const ld  prec = .000001;
+
+#define enld endl
+#define endl '\n'
+#define pb push_back
+#define debug(x) cout<<#x<<" -> "<<x<<'\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define uni(x) (x).erase(unique(all(x)), (x).end())
+#define rep(i, n) for (ll i = 0; i < (ll)(n); ++i)
+#define rep1(i, n) for (ll i = 1; i <= (ll)(n); ++i)
+#define umap unordered_map
+#define uset unordered_set
+
+template<class TIn>
+using indexed_set = tree<
+        TIn, null_type, less<TIn>,
+        rb_tree_tag, tree_order_statistics_node_update>;
+
+void nextpow(ll& n) {
+    n--;
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    n |= n >> 8;
+    n |= n >> 16;
+    n++;
 }
 
-ll inverse(ll n, ll mod)
-{
-  ll s, t;
-  gcd(n, mod, s, t);
-  return (s > 0) ? s : s + mod;
-}
+void fft(valarray<complex<double>>& x) {
+    ll s = x.size();
 
-// FFT code taken from http://e-maxx.ru/algo/fft_multiply
-
-const ll mod = 5 * (1 << 25) + 1;
-const ll root = 243;
-const ll root_1 = 114609789;
-const ll root_pw = 1<<25;
- 
-void fft (vector<ll> & a, bool invert) {
-  int n = (int) a.size();
- 
-  for (int i=1, j=0; i<n; ++i) {
-    int bit = n >> 1;
-    for (; j>=bit; bit>>=1)
-      j -= bit;
-    j += bit;
-    if (i < j)
-      swap (a[i], a[j]);
-  }
- 
-  for (int len=2; len<=n; len<<=1) {
-    ll wlen = invert ? root_1 : root;
-    for (ll i=len; i<root_pw; i<<=1)
-      wlen = ll (wlen * 1ll * wlen % mod);
-    for (int i=0; i<n; i+=len) {
-      ll w = 1;
-      for (int j=0; j<len/2; ++j) {
-	ll u = a[i+j],  v = ll (a[i+j+len/2] * 1ll * w % mod);
-	a[i+j] = u+v < mod ? u+v : u+v-mod;
-	a[i+j+len/2] = u-v >= 0 ? u-v : u-v+mod;
-	w = ll (w * 1ll * wlen % mod);
-      }
+    if (s <= 1) {
+        return;
     }
-  }
-  if (invert) {
-    ll nrev = inverse (n, mod);
-    for (int i=0; i<n; ++i)
-      a[i] = ll (a[i] * 1ll * nrev % mod);
-  }
+
+    valarray<complex<double>> even = x[slice(0, s/2, 2)];
+    valarray<complex<double>> odd  = x[slice(1, s/2, 2)];
+
+    fft(even);
+    fft(odd);
+
+    for(ll i = 0; i < s/2; i++) {
+        complex<double> t = polar(1.0, -2.0 * M_PI * i / s) * odd[i];
+        x[i]     = even[i] + t;
+        x[i+s/2] = even[i] - t;
+    }
 }
 
-const int MAX_LEN = 1000000;
+void inverse_fft(valarray<complex<double>>& x) {
+    x = x.apply(conj);
+    fft(x);
+    x = x.apply(conj);
+    x /= x.size();
+}
 
-int main()
-{
+void k_inversions(string &s) {
+    ll size = s.size();
+    size++;
+    nextpow(size);
+    size++;
+    nextpow(size);
+
+    valarray<complex<double>> poly1(size);
+    valarray<complex<double>> poly2(size);
+
+    for (ll i = 0; i < s.size(); i++) {
+        poly1[i] = (s[i] - 'A')^1;
+        poly2[size-i-1] = (s[i] - 'A');
+    }
+
+    fft(poly1);
+    fft(poly2);
+
+    for (ll i = 0; i < size; i++)
+        poly1[i] *= poly2[i];
+    inverse_fft(poly1);
+
+    for (int i = 0; i < s.size()-1; i++)
+        cout << (int)(poly1[i].real()+0.1) << endl;
+}
+
+
+int main() {
   string s;
   cin >> s;
 
-  int n = s.length();
-
-  int fft_len = 1;
-  while (fft_len < 2*MAX_LEN)
-    fft_len *= 2;
-  
-  vector<ll> A(fft_len, 0), B(fft_len, 0), C(fft_len, 0);
-  for (int i = 0; i < n; i++) {
-    if (s[i] == 'A') A[i] = 1;
-    else B[n-i] = 1;
-  }
-  
-  fft(A, false);
-  fft(B, false);
-
-  for (int i = 0; i < fft_len; i++) {
-    C[i] = A[i] * B[i] % mod;
-  }
-
-  fft(C, true);
-  for (int i = n+1; i < 2*n; i++) {
-    cout << C[i] << endl;
-  }
-  cout << endl;
-  
-  return 0;
+  k_inversions(s);
 }
---
+++ numeical_algorithms/lu_factorization.cpp
@@ -0,0 +1,118 @@
+/* This class computes the LU factorization of an n * n square matrix
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class LUFactorizer {
+    public:
+    LUFactorizer() { }
+
+    bool fit(vector<vector<double>> &A) {
+        int n = A.size();
+        for (int k = 0; k < n-1; ++k) {         // Loop over columns
+            if (A[k][k] == 0)                   // Pivot is zero
+                return false;
+            
+            for (int i = k+1; i < n; ++i)
+                A[i][k] = A[i][k] / A[k][k];    // Compute the multiplier
+
+            for (int j = k+1; j < n; ++j)
+                for (int i = k+1; i < n; ++i)   // Apply transformation to the remaining sub-matrix
+                    A[i][j] = A[i][j] - A[i][k] * A[k][j];
+        }
+        return true;
+    }
+
+    bool solve_for_b(vector<vector<double>> &A, vector<double> &b, vector<double> &x) {
+        int n = A.size();
+        vector<double> y(n, 0);
+        forward_substitution(A, b, y);          // Solve Ly = b by forward substitution
+        backward_substitution(A, y, x);         // Solve Ux = y by backward substitution
+        return true;
+    }
+
+    double determinant(vector<vector<double>> &A) {
+        double det = 1;
+        int n = A.size();
+        for (int i = 0; i < n; ++i)
+            det *= A[i][i];
+        return det;
+    }
+
+    bool forward_substitution(vector<vector<double>> &A, vector<double> &b, vector<double> &y) {
+        int n = A.size();
+        for (int j = 0; j < n; ++j) {       // Loop over the columns
+            // Compute the solution component.
+            // This is always divided by 1 as diagnols of L are 1
+            y[j] = b[j];
+
+            for (int i = j+1; i < n; ++i)   // Update the RHS
+                b[i] = b[i] - A[i][j] * y[j];
+        }
+        return true;
+    }
+
+    bool backward_substitution(vector<vector<double>> &A, vector<double> &y, vector<double> &x) {
+        int n = A.size();
+
+        for (int j = n-1; j >=0; --j) {     // Loop backward over columns
+            x[j] = y[j] / A[j][j];          // Compute the solution component
+
+            for (int i = 0; i < j; ++i)   // Update RHS
+                y[i] = y[i] - A[i][j] * x[j];
+        }
+        return true;
+    }
+
+    void print_lu(vector<vector<double>> &A) {
+        int n = A.size();
+
+        // Print L
+        cout << "L:" << endl;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == j)
+                    cout << 1 << "    ";
+                else if (i < j)
+                    cout << 0 << "    ";
+                else
+                    cout << A[i][j] << "    ";
+            }
+            cout << endl;
+        }
+
+        // Print U
+        cout << "U:" << endl;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i > j)
+                    cout << 0 << "    ";
+                else
+                    cout << A[i][j] << "    ";
+            }
+            cout << endl;
+        }
+    }
+
+    void print_vector(vector<double> &x) {
+        for (int i = 0; i < x.size(); ++i)
+            cout << x[i] << "  ";
+        cout << endl;
+    }
+};
+
+int main() {
+    //vector<vector<double>> A = {{1, 2, 2}, {4, 4, 2}, {4, 6, 4}};
+    vector<double> b = {3, 6, 10};
+    vector<double> x(3);
+    vector<vector<double>> A = {{4, 4}, {12, 7}};
+    LUFactorizer luf;
+    luf.fit(A);
+    //luf.print_lu(A);
+    cout << luf.determinant(A) << endl;
+    // luf.solve_for_b(A, b, x);
+    // luf.print_vector(x);
+}
